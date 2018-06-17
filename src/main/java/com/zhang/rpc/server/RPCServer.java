805a153d9083f903af9b17293dff17f5d371783a
gongfu <fkoff@163.com>
2018-06-18 00:04:02 +0800

    0617update

--- src/main/java/com/zhang/rpc/server/RPCServer.java
+++ src/main/java/com/zhang/rpc/server/RPCServer.java
@@ -3,12 +3,17 @@ package com.zhang.rpc.server;
 import com.zhang.rpc.server.netty.ServerNetty;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -27,6 +32,7 @@ public class RPCServer implements ApplicationContextAware, InitializingBean {
 
     private void startServer() {
         Map objMap = loadBusiBeans();
+        login2Resitry();
         RPCServerContext sc = RPCServerContext.getInstance()
                 .setPort(8888)
                 //.setServiceBean(objMap)
@@ -37,10 +43,27 @@ public class RPCServer implements ApplicationContextAware, InitializingBean {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        login2Resitry();
+
     }
 
     private void login2Resitry() {
+        try {
+            ZooKeeper zk = new ZooKeeper("c1:2181", 3000, new Watcher() {
+                @Override
+                public void process(WatchedEvent event) {
+                    System.out.println(event);
+                }
+            });
+            byte[] zkData = zk.getData("/zhangc", false, null);
+            System.out.println("aaa "+zkData);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (KeeperException e) {
+            e.printStackTrace();
+        }
+
     }
 
     /**
